847ecca748bb51c79ae903436a27cb510475cd80
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-05 14:44:00 +0300

    Added server

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+module.exports = `
+  type Query {
+    hi: String
+  }
+`;
---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from "graphql-tools";
+import { fileLoader, mergeTypes, mergeResolvers } from "merge-graphql-schemas";
+
+const PORT = 8080;
+const GRAPHQL_ENDPOINT = "/graphql";
+
+// const typeDefs = mergeTypes(fileLoader(path.join(__dirname, "./schemas")));
+// const resolvers = mergeResolvers(fileLoader(path.join(__dirname, "./resolvers")));
+const typeDefs = require("./schemas");
+const resolvers = require("./resolvers");
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+const app = express();
+
+app.use(GRAPHQL_ENDPOINT, bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: GRAPHQL_ENDPOINT }));
+
+app.get("/api/hello", (req, res) => {
+  res.send({ express: "Hello From Express" });
+});
+
+app.listen(PORT);
