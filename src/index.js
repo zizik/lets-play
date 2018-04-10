b078a935d93e4380a650a3830f7e041ee163dc65
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-10 20:03:48 +0300

    Added apollo-client

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Routes";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+import Routes from "./Routes";
 import registerServiceWorker from "./registerServiceWorker";
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
