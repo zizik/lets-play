a425935e78e1d5009a74449ecc97e84df67fe84d
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-10 21:40:32 +0300

    Added creating new user

--- src/Routes/Components/Users.js
+++ src/Routes/Components/Users.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
-import { Container, Header, Input, Button, Form } from "semantic-ui-react";
+import { Container, Header, Input, Button, Form, Divider } from "semantic-ui-react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class Users extends Component {
+  state = {
+    // users: [],
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  onSubmit = async () => {
+    const { name, email, password } = this.state;
+    const res = await this.props.mutate({
+      variables: { name, email, password },
+    });
+    console.log(res);
+  };
+
   render() {
     return (
       <Container text>
-        <Header as="h2">Users</Header>
+        <Header as="h1">Users</Header>
+        <Button primary onClick={this.getAllUsers}>
+          Show All Users
+        </Button>
+        <Divider />
         <Form>
+          <Header as="h2">Create User</Header>
           <Form.Field>
-            <Input onChange={this.onChange} name="username" placeholder="Username" fluid />
+            <Input onChange={this.onChange} name="name" placeholder="Username" fluid />
           </Form.Field>
           <Form.Field>
             <Input onChange={this.onChange} name="email" placeholder="Email" fluid />
@@ -25,4 +52,10 @@ class Users extends Component {
   }
 }
 
-export default Users;
+const registerMutation = gql`
+  mutation($name: String!, $email: String!, $password: String!) {
+    createUser(name: $name, email: $email, password: $password)
+  }
+`;
+
+export default graphql(registerMutation)(Users);
