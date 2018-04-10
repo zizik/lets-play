058e22f509d03786aac922d637ae15df91cd4958
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-10 20:05:50 +0300

    Added apollo-link-http

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 
 import Routes from "./Routes";
 import registerServiceWorker from "./registerServiceWorker";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/graphql",
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const App = () => (
   <ApolloProvider client={client}>
