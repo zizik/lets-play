c7ed85a360dbab48ff5870ce861480e539ca487e
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-11 22:40:03 +0300

    Removed games from state

--- src/Routes/Components/Games.js
+++ src/Routes/Components/Games.js
@@ -5,7 +5,6 @@ import { graphql, compose } from "react-apollo";
 
 class Games extends Component {
   state = {
-    games: [],
     name: "",
     icon: "",
   };
@@ -17,40 +16,25 @@ class Games extends Component {
 
   onSubmit = async () => {
     const { name, icon } = this.state;
-    const res = await this.props.createGameMutation({
+    await this.props.createGameMutation({
       variables: { name, icon: icon || "No Icon" },
     });
-    console.log(res);
-  };
-
-  getAllGames = () => {
-    const { getAllGames: games } = this.props.games;
-    this.setState({ games });
   };
 
   render() {
-    const allGames = this.state.games.map(user => (
-      <Table.Row key={user.id}>
-        <Table.Cell>{user.name}</Table.Cell>
-        <Table.Cell>{user.icon}</Table.Cell>
-      </Table.Row>
-    ));
+    const { loading, getAllGames: games = [] } = this.props.games;
+    const allGames =
+      !loading &&
+      games.map(user => (
+        <Table.Row key={user.id}>
+          <Table.Cell>{user.name}</Table.Cell>
+          <Table.Cell>{user.icon}</Table.Cell>
+        </Table.Row>
+      ));
+
     return (
       <Container text>
         <Header as="h1">Games</Header>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Game Name</Table.HeaderCell>
-              <Table.HeaderCell>Game Icon</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>{allGames}</Table.Body>
-        </Table>
-        <Button primary onClick={this.getAllGames}>
-          Show All Users
-        </Button>
-        <Divider />
         <Form>
           <Header as="h2">Create Game</Header>
           <Form.Field>
@@ -63,6 +47,16 @@ class Games extends Component {
             Submit
           </Button>
         </Form>
+        <Divider />
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Game Name</Table.HeaderCell>
+              <Table.HeaderCell>Game Icon</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>{allGames}</Table.Body>
+        </Table>
       </Container>
     );
   }
@@ -80,7 +74,11 @@ const getAllGames = gql`
 
 const createGameMutation = gql`
   mutation($name: String!, $icon: String) {
-    createGame(name: $name, icon: $icon)
+    createGame(name: $name, icon: $icon) {
+      id
+      name
+      icon
+    }
   }
 `;
 
