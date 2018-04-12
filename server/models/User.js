2228b64160b1982c91fd74ed2914daf0fca61b17
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-13 01:17:48 +0300

    Added user model validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,37 @@
 export default (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: "The username can only contain letters and numbers",
+        },
+        len: {
+          args: [3, 25],
+          msg: "The username needs to be between 3 and 25 characters long",
+        },
+      },
+    },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Invalid email",
+        },
+      },
     },
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [5, 100],
+          msg: "The password needs to be between 5 and 100 characters long",
+        },
+      },
+    },
   });
   User.associate = models => {
     User.belongsToMany(models.Invite, {
