3534c811eec34555101aab7e78766284b8affa58
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-12 22:27:05 +0300

    Changed Invites component to use formik

--- src/Routes/Components/Invites.js
+++ src/Routes/Components/Invites.js
@@ -1,73 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container, Header, Input, Button, Form, Divider, Table } from "semantic-ui-react";
 import gql from "graphql-tag";
 import { graphql, compose } from "react-apollo";
+import { withFormik } from "formik";
 
-class Invites extends Component {
-  state = {
-    description: "",
-    userId: 1,
-    gameId: 1,
-  };
+const Invites = ({ getAllInvites: { loading, getAllInvites: invites }, submitForm, handleChange, values, ...props }) => {
+  // const { loading, getAllInvites: invites } = getAllInvites;
+  const allInvites =
+    !loading &&
+    invites.map(user => (
+      <Table.Row key={user.id}>
+        <Table.Cell>{user.description}</Table.Cell>
+        <Table.Cell>{user.userId}</Table.Cell>
+        <Table.Cell>{user.gameId}</Table.Cell>
+      </Table.Row>
+    ));
 
-  onChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  onSubmit = async () => {
-    const { description, userId, gameId } = this.state;
-    await this.props.createInviteMutation({
-      variables: { description, userId, gameId },
-    });
-    await this.props.getAllInvites.refetch();
-  };
-
-  render() {
-    const { loading, getAllInvites: invites } = this.props.getAllInvites;
-    const allInvites =
-      !loading &&
-      invites.map(user => (
-        <Table.Row key={user.id}>
-          <Table.Cell>{user.description}</Table.Cell>
-          <Table.Cell>{user.userId}</Table.Cell>
-          <Table.Cell>{user.gameId}</Table.Cell>
-        </Table.Row>
-      ));
-
-    return (
-      <Container text>
-        <Header as="h1">Invites</Header>
-        <Form>
-          <Header as="h2">Create Invite</Header>
-          <Form.Field>
-            <Input onChange={this.onChange} name="description" placeholder="Invite description" fluid />
-          </Form.Field>
-          <Form.Field>
-            <Input onChange={this.onChange} name="userId" placeholder="User Id" fluid />
-          </Form.Field>
-          <Form.Field>
-            <Input onChange={this.onChange} name="gameId" placeholder="Game Id" fluid />
-          </Form.Field>
-          <Button primary onClick={this.onSubmit}>
-            Submit
-          </Button>
-        </Form>
-        <Divider />
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Invite description</Table.HeaderCell>
-              <Table.HeaderCell>User Id</Table.HeaderCell>
-              <Table.HeaderCell>Game Id</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>{allInvites}</Table.Body>
-        </Table>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container text>
+      <Header as="h1">Invites</Header>
+      <Form>
+        <Header as="h2">Create Invite</Header>
+        <Form.Field>
+          <Input onChange={handleChange} value={values.description} name="description" placeholder="Invite description" fluid />
+        </Form.Field>
+        <Form.Field>
+          <Input onChange={handleChange} value={values.userId} type="number" name="userId" placeholder="User Id" fluid />
+        </Form.Field>
+        <Form.Field>
+          <Input onChange={handleChange} value={values.gameId} type="number" name="gameId" placeholder="Game Id" fluid />
+        </Form.Field>
+        <Button primary onClick={submitForm}>
+          Submit
+        </Button>
+      </Form>
+      <Divider />
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Invite description</Table.HeaderCell>
+            <Table.HeaderCell>User Id</Table.HeaderCell>
+            <Table.HeaderCell>Game Id</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>{allInvites}</Table.Body>
+      </Table>
+    </Container>
+  );
+};
 
 const getAllInvites = gql`
   query {
@@ -86,6 +66,20 @@ const createInviteMutation = gql`
   }
 `;
 
-export default compose(graphql(getAllInvites, { name: "getAllInvites" }), graphql(createInviteMutation, { name: "createInviteMutation" }))(
-  Invites,
-);
+export default compose(
+  graphql(getAllInvites, { name: "getAllInvites" }),
+  graphql(createInviteMutation, { name: "createInviteMutation" }),
+  withFormik({
+    mapPropsToValues: props => ({
+      description: "",
+      userId: 1,
+      gameId: 1,
+    }),
+    handleSubmit: async (values, { props }) => {
+      await props.createInviteMutation({
+        variables: values,
+      });
+      await props.getAllInvites.refetch();
+    },
+  }),
+)(Invites);
