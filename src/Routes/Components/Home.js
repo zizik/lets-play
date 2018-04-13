151a5922110da03c5f1a39e46394888dadb380ce
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-14 01:39:42 +0300

    Fixed home component

--- src/Routes/Components/Home.js
+++ src/Routes/Components/Home.js
@@ -6,7 +6,11 @@ const ListExampleBasic = () => (
   <List>
     <List.Item>
       <Link to="/users"> Users </Link>
+    </List.Item>
+    <List.Item>
       <Link to="/games"> Games </Link>
+    </List.Item>
+    <List.Item>
       <Link to="/invites"> Invites </Link>
     </List.Item>
   </List>
