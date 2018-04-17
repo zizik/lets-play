c8c719f67ffff5b61e17b6656174ef888d0c2d0b
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-17 09:34:20 +0300

    Added refreshToken to header

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -5,15 +5,12 @@ import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({ uri: "/graphql" });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+const authLink = setContext(() => ({
+  headers: {
+    "x-token": localStorage.getItem("token"),
+    "x-refresh-token": localStorage.getItem("refreshToken"),
+  },
+}));
 
 export default new ApolloClient({
   link: authLink.concat(httpLink),
