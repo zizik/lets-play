2ba6e680c059f43b9ac99f181b9d033f4cb3f5e9
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-17 09:27:55 +0300

    Added token to header

---
+++ src/apolloClient.js
@@ -0,0 +1,21 @@
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+
+const httpLink = createHttpLink({ uri: "/graphql" });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+export default new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 
 import Routes from "./Routes";
 import registerServiceWorker from "./registerServiceWorker";
-
-const client = new ApolloClient({
-  link: createHttpLink({ uri: "/graphql" }),
-  cache: new InMemoryCache(),
-});
+import client from "./apolloClient";
 
 const App = () => (
   <ApolloProvider client={client}>
