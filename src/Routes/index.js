09cd034834baceefc141a8efa9165e4b0b39296f
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-19 12:35:57 +0300

    Added header component

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 
+import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Users from "./Components/Users";
 import Games from "./Components/Games";
@@ -9,9 +10,10 @@ import Invites from "./Components/Invites";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
 
-export default () => (
+export default props => (
   <BrowserRouter>
     <Switch>
+      <Header />
       <Route path="/" exact component={Home} />
       <Route path="/users" exact component={Users} />
       <Route path="/games" exact component={Games} />
