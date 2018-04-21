3b034d6bf3e9059d43963ffccbdfd265ca36948b
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-21 18:29:53 +0300

    Changed inviteItems to be component

--- src/Routes/Components/InviteItem.js
+++ src/Routes/Components/InviteItem.js
@@ -3,16 +3,21 @@ import { ListItem, ListItemText, ListItemSecondaryAction } from "material-ui/Lis
 import IconButton from "material-ui/IconButton";
 import DeleteIcon from "material-ui-icons/Delete";
 
-export default ({ invite: { game } }) => {
-  return (
-    <ListItem button divider>
-      {/* <ListItemAvatar><GameIcon game="Overwatch" /></ListItemAvatar> */}
-      <ListItemText primary={game.name} secondary="24:00:00" />
-      <ListItemSecondaryAction>
-        <IconButton aria-label="Delete" onClick={this.handleDelete}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
-  );
-};
+class Invite extends React.Component {
+  render() {
+    const { invite: { game } } = this.props;
+    return (
+      <ListItem button divider>
+        {/* <ListItemAvatar><GameIcon game="Overwatch" /></ListItemAvatar> */}
+        <ListItemText primary={game.name} secondary="24:00:00" />
+        <ListItemSecondaryAction>
+          <IconButton aria-label="Delete" onClick={this.handleDelete}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  }
+}
+
+export default Invite;
