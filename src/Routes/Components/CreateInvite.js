478451f5c3750dec35ad5f7a20975e80f878ca9a
Zizidukabrik <zizidukabrik@gmail.com>
2018-04-26 17:40:00 +0300

    Added select expired time in create invite

--- src/Routes/Components/CreateInvite.js
+++ src/Routes/Components/CreateInvite.js
@@ -3,6 +3,8 @@ import Input, { InputLabel } from "material-ui/Input";
 import { FormControl } from "material-ui/Form";
 import Typography from "material-ui/Typography";
 import Button from "material-ui/Button";
+import Select from "material-ui/Select";
+import { MenuItem } from "material-ui/Menu";
 import { graphql, compose } from "react-apollo";
 import { withFormik } from "formik";
 import styled from "styled-components";
@@ -28,6 +30,14 @@ const Invites = ({ handleSubmit, handleChange, values }) => (
       <InputLabel htmlFor="gameId">Game Id</InputLabel>
       <Input id="gameId" value={values.gameId} onChange={handleChange} />
     </FormControl>
+    <FormControl margin="normal" fullWidth>
+      <InputLabel htmlFor="expiredTime">Expired Time</InputLabel>
+      <Select id="expiredTime" name="expiredTime" value={values.expiredAt} onChange={handleChange}>
+        <MenuItem value={5}>5</MenuItem>
+        <MenuItem value={7}>7</MenuItem>
+        <MenuItem value={10}>10</MenuItem>
+      </Select>
+    </FormControl>
     <StyledButton onClick={handleSubmit} color="primary" variant="raised">
       Submit
     </StyledButton>
@@ -49,6 +59,7 @@ export default compose(
     mapPropsToValues: props => ({
       description: "",
       gameId: 1,
+      expiredAt: 7,
     }),
     handleSubmit: async (values, { props }) => {
       const { data: { createInvite } } = await props.createInviteMutation({
