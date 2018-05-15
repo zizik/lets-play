3dce41f8942428532e6d984049589336fa112ed0
Zizidukabrik <zizidukabrik@gmail.com>
2018-05-16 02:04:59 +0300

    Added error when user try to like his own invites

--- server/resolvers/likeStatus.js
+++ server/resolvers/likeStatus.js
@@ -20,21 +20,25 @@ export default {
   Mutation: {
     createLikeStatus: async (parent, { inviteId }, { models, user }) => {
       try {
+        const { userId: owner } = await models.Invite.findById(inviteId, { raw: true });
+        if (owner === user.id) {
+          throw new Error("You can't like your own invites");
+        }
         await models.LikeStatus.create({ inviteId, userId: user.id });
         return {
           ok: true,
         };
       } catch (err) {
+        console.log(err);
         if (err.name === "SequelizeUniqueConstraintError") {
           return {
             ok: false,
             errors: [{ reason: "createLikeStatus", message: "You already like this invite" }],
           };
         }
-        console.log(err);
         return {
           ok: false,
-          errors: [{ reason: "createLikeStatus", message: "Cant add like" }],
+          errors: [{ reason: "createLikeStatus", message: err.message }],
         };
       }
     },
--- src/Queries/LikeStatus.js
+++ src/Queries/LikeStatus.js
@@ -1,5 +1,6 @@
 import gql from "graphql-tag";
 
+// eslint-disable-next-line import/prefer-default-export
 export const ADD_LIKE_STATUS = gql`
   mutation($inviteId: Int!) {
     createLikeStatus(inviteId: $inviteId) {
